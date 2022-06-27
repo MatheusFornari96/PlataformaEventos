604b598dfb6f514bcb025421ad919442da37931e
Matheus Fornari <fornari@fag.edu.br>
2022-06-26 21:30:44 -0300

    criado sidebar e conectadol na api

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface lesson{
-  id: string;
-  title: string;
-}
+import { Evento } from "./pages/Evento"
 
 function App() {
-  // Forma comum de requisção para api
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY,
-  //   }).then(response => console.log(response.data));
-  // },[])
-
-
-  const {data} = useQuery<{lessons: lesson[]}>(GET_LESSONS_QUERY);
-  console.log(data);
-
   return (
-   <ul>
-    {data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title}</li>
-    })}
-   </ul>
+    <Evento />
   )
 }
 
